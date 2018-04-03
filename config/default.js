62669acd4eb70766186c28aa142add9b954877b8
shaohao <951389880@qq.com>
2018-04-03 15:25:51 +0800

    update images and config

--- config/default.js
+++ config/default.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongodb: 'mongodb://localhost:27018/ztNet',
-  port: '8089'
+  port: '8089',
+  hostname: '127.0.0.1'
 }
--- config/production.js
+++ config/production.js
@@ -1,3 +1,5 @@
 module.exports = {
-  
+  mongodb: 'mongodb://localhost:27017/ztNet',
+  port: '3000',
+  hostname: '0.0.0.0'
 }
